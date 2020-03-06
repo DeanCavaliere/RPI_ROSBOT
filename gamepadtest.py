d0facab13ff05dcb80e843496c1171354c13a0b5
Dean Cavaliere <49283626+DeanCavaliere@users.noreply.github.com>
2020-03-06 17:07:47 -0500

    New Update

--- gamepadtest.py
+++ gamepadtest.py
@@ -1,71 +1,73 @@
-import evdev
-from evdev import InputDevice, categorize, ecodes
 
-#creates object 'gamepad' to store the data
-#you can call it whatever you like
-gamepad = InputDevice('/dev/input/event7')
+def controller():
+    import evdev
+    from evdev import InputDevice, categorize, ecodes
 
-xBtn = 304
-oBtn = 305
-sqBtn = 308
-triBtn = 307
+    #creates object 'gamepad' to store the data
+    #you can call it whatever you like
+    gamepad = InputDevice('/dev/input/event7')
 
-lTrB = 312
-lTrT = 310
-rTrB = 313
-rTrT = 311
+    xBtn = 304
+    oBtn = 305
+    sqBtn = 308
+    triBtn = 307
 
-lAlgLR = ecodes.ABS_X
-lAlgUD = ecodes.ABS_Y
-rAlgLR = ecodes.ABS_RX
-rAlgUD = ecodes.ABS_RY
+    lTrB = 312
+    lTrT = 310
+    rTrB = 313
+    rTrT = 311
 
-#prints out device info at start
-print(gamepad)
+    lAlgLR = ecodes.ABS_X
+    lAlgUD = ecodes.ABS_Y
+    rAlgLR = ecodes.ABS_RX
+    rAlgUD = ecodes.ABS_RY
 
-#loop and filter by event code and print the mapped label
-for event in gamepad.read_loop():
-    if event.type == evdev.ecodes.EV_KEY:
-        if event.value == 1:
-            if event.code == xBtn:
-                print("X")
-            elif event.code == oBtn:
-                print("O")
-            elif event.code == sqBtn:
-                print("Square")
-            elif event.code == triBtn:
-                print("Triangle")
-            elif event.code == 314:
-                print("Share")
-            elif event.code == 315:
-                print("Options")
-            elif event.code == rTrT:
-                print("Right Top BTN ")
-            elif event.code == rTrB:
-                print("Right Bottom BTN ")
-            elif event.code == lTrT:
-                print("Left Top BTN ")
-            elif event.code == lTrB:
-                print("Left Bottom BTN ")
-            elif event.code == 316:
-                print("PS BTN")
+    #prints out device info at start
+    print(gamepad)
+
+    #loop and filter by event code and print the mapped label
+    for event in gamepad.read_loop():
+        if event.type == evdev.ecodes.EV_KEY:
+            if event.value == 1:
+                if event.code == xBtn:
+                    print("X")
+                elif event.code == oBtn:
+                    print("O")
+                elif event.code == sqBtn:
+                    print("Square")
+                elif event.code == triBtn:
+                    print("Triangle")
+                elif event.code == 314:
+                    print("Share")
+                elif event.code == 315:
+                    print("Options")
+                elif event.code == rTrT:
+                    print("Right Top BTN ")
+                elif event.code == rTrB:
+                    print("Right Bottom BTN ")
+                elif event.code == lTrT:
+                    print("Left Top BTN ")
+                elif event.code == lTrB:
+                    print("Left Bottom BTN ")
+                elif event.code == 316:
+                    print("PS BTN")
                 
-    if event.type == evdev.ecodes.EV_ABS:
-        if event.value < 110:
-            if event.code == lAlgUD:
-                print('Left Analog Up '+str(event.value))
-            elif event.code == lAlgLR:
-                print("Left Analog Left "+str(event.value))
-            elif event.code == rAlgUD:
-                print("Right Analog Up "+str(event.value))
-            elif event.code == rAlgLR:
-                print("Right Analog Left "+str(event.value))
-        if event.value > 146:
-            if event.code == lAlgUD:
-                print("Left Analog Down "+ str(event.value))
-            elif event.code == lAlgLR:
-                print("Left Analog Right "+str(event.value))
-            elif event.code == rAlgUD:
-                print("Right Analog Down "+str(event.value))
-            elif event.code == rAlgLR:
-                print("Right Analog Right "+str(event.value))
+        if event.type == evdev.ecodes.EV_ABS:
+            if event.value < 110:
+                if event.code == lAlgUD:
+                    print('Left Analog Up '+str(event.value))
+                elif event.code == lAlgLR:
+                    print("Left Analog Left "+str(event.value))
+                elif event.code == rAlgUD:
+                    print("Right Analog Up "+str(event.value))
+                elif event.code == rAlgLR:
+                    print("Right Analog Left "+str(event.value))
+            if event.value > 146:
+                if event.code == lAlgUD:
+                    print("Left Analog Down "+ str(event.value))
+                elif event.code == lAlgLR:
+                    print("Left Analog Right "+str(event.value))
+                elif event.code == rAlgUD:
+                    print("Right Analog Down "+str(event.value))
+                elif event.code == rAlgLR:
+                    print("Right Analog Right "+str(event.value))
