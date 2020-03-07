922179cfdef58f8a23da6f50adf60fd97365b997
Dean Cavaliere <49283626+DeanCavaliere@users.noreply.github.com>
2020-03-07 11:24:16 -0500

    New Update

--- gamepadtest.py
+++ gamepadtest.py
@@ -58,70 +58,73 @@ print(gamepad)
 
 # loop and filter by event code and print the mapped label
 for event in gamepad.read_loop():
-    if event.type == evdev.ecodes.EV_KEY:
-        if event.value == 1:
-            if event.code == xBtn:
-                data =  "X"
-                mqttPublish(data)
-            elif event.code == oBtn:
-                data = ("O")
-                mqttPublish(data)
-            elif event.code == sqBtn:
-                data = ("Square")
-                mqttPublish(data)
-            elif event.code == triBtn:
-                data = ("Triangle")
-                mqttPublish(data)
-            elif event.code == 314:
-                data = ("Share")
-                mqttPublish(data)
-            elif event.code == 315:
-                data = ("Options")
-                mqttPublish(data)
-            elif event.code == rTrT:
-                data = ("Right Top BTN")
-                mqttPublish(data)
-            elif event.code == rTrB:
-                data = ("Accel On")
-                mqttPublish(data)
-            elif event.code == lTrT:
-                data = ("Left Top BTN")
-                mqttPublish(data)
-            elif event.code == lTrB:
-                data = ("Left Bottom BTN")
-                mqttPublish(data)
-            elif event.code == 316:
-                data = ("PS BTN")
-                mqttPublish(data)
-        if event.value == 0:
-            if event.code == rTrB:
-                data = ("Accel Off")
-                mqttPublish(data)
+    try:
+        if event.type == evdev.ecodes.EV_KEY:
+            if event.value == 1:
+                if event.code == xBtn:
+                    data =  "X"
+                    mqttPublish(data)
+                elif event.code == oBtn:
+                    data = ("O")
+                    mqttPublish(data)
+                elif event.code == sqBtn:
+                    data = ("Square")
+                    mqttPublish(data)
+                elif event.code == triBtn:
+                    data = ("Triangle")
+                    mqttPublish(data)
+                elif event.code == 314:
+                    data = ("Share")
+                    mqttPublish(data)
+                elif event.code == 315:
+                    data = ("Options")
+                    mqttPublish(data)
+                elif event.code == rTrT:
+                    data = ("Right Top BTN")
+                    mqttPublish(data)
+                elif event.code == rTrB:
+                    data = ("Accel On")
+                    mqttPublish(data)
+                elif event.code == lTrT:
+                    data = ("Left Top BTN")
+                    mqttPublish(data)
+                elif event.code == lTrB:
+                    data = ("Left Bottom BTN")
+                    mqttPublish(data)
+                elif event.code == 316:
+                    data = ("PS BTN")
+                    mqttPublish(data)
+            if event.value == 0:
+                if event.code == rTrB:
+                    data = ("Accel Off")
+                    mqttPublish(data)
 
-    if event.type == evdev.ecodes.EV_ABS:
-        if event.value < 2:
-            if event.code == lAlgUD:
-                data = ('Left Analog Up')  # +str(event.value))
-                mqttPublish(data)
-            elif event.code == lAlgLR:
-                data = ("Left Analog Left")  # +str(event.value))
-                mqttPublish(data)
-            elif event.code == rAlgUD:
-                data = ("Right Analog Up")  # +str(event.value))
-                mqttPublish(data)
-            elif event.code == rAlgLR:
-                data = ("Right Analog Left")  # +str(event.value))
-                mqttPublish(data)
-        if event.value > 253:
-            if event.code == lAlgUD:
-                data = ("Left Analog Down")  # + str(event.value))
-                mqttPublish(data)
-            elif event.code == lAlgLR:
-                data = ("Left Analog Right")  # +str(event.value))
-                mqttPublish(data)
-            elif event.code == rAlgUD:
-                data = ("Right Analog Down")  # +str(event.value))
-                mqttPublish(data)
-            elif event.code == rAlgLR:
-                data = ("Right Analog Right")  # +str(event.value))
-                mqttPublish(data)
+        if event.type == evdev.ecodes.EV_ABS:
+            if event.value < 2:
+                if event.code == lAlgUD:
+                    data = ('Left Analog Up')  # +str(event.value))
+                    mqttPublish(data)
+                elif event.code == lAlgLR:
+                    data = ("Left Analog Left")  # +str(event.value))
+                    mqttPublish(data)
+                elif event.code == rAlgUD:
+                    data = ("Right Analog Up")  # +str(event.value))
+                    mqttPublish(data)
+                elif event.code == rAlgLR:
+                    data = ("Right Analog Left")  # +str(event.value))
+                    mqttPublish(data)
+            if event.value > 253:
+                if event.code == lAlgUD:
+                    data = ("Left Analog Down")  # + str(event.value))
+                    mqttPublish(data)
+                elif event.code == lAlgLR:
+                    data = ("Left Analog Right")  # +str(event.value))
+                    mqttPublish(data)
+                elif event.code == rAlgUD:
+                    data = ("Right Analog Down")  # +str(event.value))
+                    mqttPublish(data)
+                elif event.code == rAlgLR:
+                    data = ("Right Analog Right")  # +str(event.value))
+                    mqttPublish(data)
+    except KeyboardInterrupt:
+        sys.exit()
